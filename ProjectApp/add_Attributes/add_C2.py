76e53cdd014d25e5fefb721c521b8d7e5b058e95
KLiehr <80169352+KLiehr@users.noreply.github.com>
2021-11-18 20:35:01 +0100

    changed def C2

--- ProjectApp/add_Attributes/add_C2.py
+++ ProjectApp/add_Attributes/add_C2.py
@@ -1,41 +1,35 @@
 import pm4py
 from pm4py.objects.log.importer.xes import importer as xes_importer
 
-
-# given an event log(sorted by timestamps!!!) and a set of activities, add NextActivity(C2) attribute to each event and then return the log
-# Definition C2: for a given activity set, says which is the next activity of the set after the event, NoSetActNext is the placeholder, if there are no set activites executed subsequently
-def add_C2(log, act_set):
+# Requires Activity
+# given an event log(sorted by timestamps!!!), add NextActivity(C2) attribute to each event and then return the log
+# Definition C2: says which is the next activity after the event, NoNextAct is the placeholder, if there are no activites executed subsequently
+def add_C2(log):
 
     for trace in log:
         for event in trace:
-            if not act_set:
-                event['NextActivity(C2)'] = "NoSetActNext"
-            else:
-                event['NextActivity(C2)'] = nextSetAct(trace, event, act_set)
-
-
+            event['NextActivity(C2)'] = nextAct(trace, event)
 
     return log
 
 # returns as string the next set activity for a given trace,event and activity set
-def nextSetAct(trace, event, act_set):
+def nextAct(trace, event):
 
     # denotes if we have passed the given activity in the trace
     after = False
 
     for ev in trace:
 
-        # check if ev's activity is in the set, if we are after the given event
+        # return events activity, if we are after the given event
         if after:
-            if ev['Activity'] in act_set:
-                return ev['Activity']
+            return ev['Activity']
 
         # have we reached the event
         if event == ev:
             after = True
 
     # returns placeholder if there is no subsequent set activity
-    return "NoSetActNext"
+    return "NoNextAct"
 
 
 
