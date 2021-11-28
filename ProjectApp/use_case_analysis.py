3714b9dbf8631ebd97d1906fbadc3e371ab1cb14
KLiehr <80169352+KLiehr@users.noreply.github.com>
2021-11-28 15:51:15 +0100

    added Regression tree and one hot encoding of only cat vars

--- ProjectApp/use_case_analysis.py
+++ ProjectApp/use_case_analysis.py
@@ -6,16 +6,15 @@ import subprocess
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
 from . import log_utils
 
 
-from sklearn.metrics import confusion_matrix 
+
 from sklearn.model_selection import train_test_split 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
-from sklearn.metrics import accuracy_score 
-from sklearn.metrics import classification_report 
+
 import graphviz
 
 
@@ -36,13 +35,27 @@ def analyze_log(log, dep_var, ind_vars):
     target_column = log_df[[dep_var]]
     print(target_column)
 
-    # Do onehotencoding:
-    one_hot_data = pd.get_dummies(data_columns)
+    # get non numerical ind-vars
+    cat_vars = []
+    for ind_var in ind_vars:
+        if not log_utils.isNumerical(data_columns, ind_var):
+            cat_vars.append(ind_var)
+    print(cat_vars)
+
+    # Do onehotencoding for any non numerical variable:
+    one_hot_data = pd.get_dummies(data_columns, columns=cat_vars)
     print(one_hot_data)
 
-    # train DecisionTree, NOT FOR CATEGORICAL VARIABLES
-    tree_clf = DecisionTreeClassifier()
-    tree_clf.fit(one_hot_data, target_column)
+    # train DecisionTree for cat target Regression tree for num target, data CANNOT BE CATEGORICAL VARIABLES
+
+    if not log_utils.isNumerical(target_column, dep_var):
+        tree_clf = DecisionTreeClassifier()
+        tree_clf.fit(one_hot_data, target_column)
+        print('Trained a decision tree!')
+    else:
+        tree_clf = DecisionTreeRegressor()
+        tree_clf.fit(one_hot_data, target_column)
+        print('Trained a regression tree!')
     
     # visualize tree
     dot_data = tree.export_graphviz(tree_clf, out_file=None, feature_names= one_hot_data.columns) 
