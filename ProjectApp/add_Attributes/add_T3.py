e5d8e1f310937a875e7925d707084e29ad724d0d
KLiehr <80169352+KLiehr@users.noreply.github.com>
2022-01-12 18:04:44 +0100

    fix T3 for start and end times

--- ProjectApp/add_Attributes/add_T3.py
+++ ProjectApp/add_Attributes/add_T3.py
@@ -10,19 +10,34 @@ from ProjectApp import log_utils
 # Definition T3: computes the activity time of the trace left from time of an event
 def add_T3(log):
     '''given an event log(sorted by timestamps!!!), add CaseTimeToEnd(T3) attribute to each event and then return the log
-            Definition T3: computes the activity time of the trace left from time of an event'''
+            Definition T3: computes the activity time of the trace left from time of an event(end time if there)'''
 
     for trace in log:
         for event in trace:  
+            if log_utils.end_time_attr == 'NO END TIME ATTRIBUTE IN LOG':
                 event['CaseTimeToEnd(T3)'] = caseTimeLeft(trace, event)
-            
+            else:
+                event['CaseTimeToEnd(T3)'] = caseTimeFromEventstart(trace, event[log_utils.end_time_attr])
+
     return log
 
+def caseTimeFromEventstart(trace, endtime):
+    '''returns the trace time left after given end time'''
 
+    # denotes the duration of activity
+    case_dur = 0
+
+    # get trace end time
+    case_end = trace[len(trace) - 1][log_utils.end_time_attr]
+
+    # compute case duration
+    case_dur = case_end - endtime
+
+    return case_dur
 
 # returns the activity time left at event time
 def caseTimeLeft(trace, event):
-    '''returns the activity time left at event time'''
+    '''returns the trace time left at event time'''
 
     # denotes the duration of activity
     case_dur = 0
